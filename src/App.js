45754cf5db1fe0b3a6a1e0d87871a81f84fcfbe9
Андрій Шевельов <ashevelevmti@gmail.com>
2022-08-10 18:47:00 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import "./App.css";
-
-function App() {
-  return <h1>Hello!</h1>;
+/** @format */
+var button = document.querySelector("button");
+var input1 = document.getElementById("num1");
+var input2 = document.getElementById("num2");
+function add(num1, num2) {
+    return num1 + num2;
 }
-
-export default App;
+button.addEventListener("click", function () {
+    console.log(add(+input1.value, +input2.value));
+});
+var age;
+var names;
+var toggle;
+var empty;
+var notInitialize;
+age = 50;
+names = "Max";
+toggle = true;
+empty = null;
+notInitialize = undefined;
