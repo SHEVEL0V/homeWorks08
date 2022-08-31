48aafb7437c05746f885ffa8d98de30654278a44
Андрій Шевельов <ashevelevmti@gmail.com>
2022-08-31 14:44:18 +0300

    ADD test

--- src/components/item.tsx
+++ src/components/item.tsx
@@ -1,6 +1,6 @@
 /** @format */
 
-import { useState } from "React";
+import React, { useState } from "react";
 
 interface IContact {
   id: string;
@@ -13,23 +13,32 @@ type Data = {
   data: IContact;
 };
 
-const Item: React.FC<Data> = ({ data }) => {
-  // const [number, setNumber] = useState(1);
+type State = string;
 
-  // const increment = () => {
-  //   setNumber(2);
-  // };
-  // const decrement = () => {
-  //   setNumber(1);
-  // };
+const Item: React.FC<Data> = ({ data }) => {
+  const { phone, email } = data;
+  const [value, setValue] = useState<State>(phone);
+  const [inpValue, setInpValue] = useState<State>("");
+
+  const onPhone = () => {
+    setValue(phone);
+  };
+  const onEmail = () => {
+    setValue(email);
+  };
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInpValue(e.target.value);
+  };
 
   return (
     <li>
       <b>{data.name}</b>
-      <h3>-{data.phone}-</h3>
+      <h3>-{value}-</h3>
 
-      {/* <button onClick={decrement}>On -</button>
-      <button onClick={increment}>On +</button> */}
+      <button onClick={onPhone}>phone</button>
+      <button onClick={onEmail}>email</button>
+      <input onChange={onChange} value={inpValue}></input>
     </li>
   );
 };
